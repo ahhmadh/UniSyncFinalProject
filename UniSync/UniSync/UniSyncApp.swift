b529d3dc20a6bc4043237642ec6d371c5e8c7c77
ahhmadh <ahmadhasssan2004@gmail.com>
2025-11-24 11:57:47 -0500

    Project Structure finished

---
+++ UniSync/UniSync/UniSyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  UniSyncApp.swift
+//  UniSync
+//
+//  Created by Ahmad Hassan on 2025-11-24.
+//
+
+import SwiftUI
+
+@main
+struct UniSyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ UniSync/UniSyncTests/UniSyncTests.swift
@@ -0,0 +1,17 @@
+//
+//  UniSyncTests.swift
+//  UniSyncTests
+//
+//  Created by Ahmad Hassan on 2025-11-24.
+//
+
+import Testing
+@testable import UniSync
+
+struct UniSyncTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
